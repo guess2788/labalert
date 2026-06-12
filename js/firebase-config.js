a6d5e0e31bfaf9caf8ca9076581ba86fdc4a2db5
Asnoun Chakib <chakibasnoun6@gmail.com>
2026-06-12 14:40:17 +0000

    Add technician login, auth guards, and Firebase Hosting config

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,7 @@
 // js/firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB4kZeQeiItAnqFmS9RMJN6RqMHEycBV6g",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
